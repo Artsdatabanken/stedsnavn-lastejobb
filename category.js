fffd5ae8899aa4ee16fb3bc41ef761ee043c1ea0
Bjorn Reppen <bjornreppen@users.noreply.github.com>
2019-08-23 10:08:45 +0200

    Categories as object for the schema

--- category.js
+++ category.js
@@ -1,13 +1,18 @@
-function add(r, category) {
-  if (r[category]) return r[category];
-  r[category] = ++r.count;
-  return r[category];
+let count = 1;
+
+function add(r, l1, l2, l3) {
+  if (!r[l1]) r[l1] = {};
+  const x1 = r[l1];
+  if (!x1[l2]) x1[l2] = {};
+  const x2 = x1[l2];
+  if (x2[l3]) return x2[l3];
+  x2[l3] = ++count;
+  return x2[l3];
 }
 
 function create() {
   const r = {
-    add: category => add(r, category),
-    count: 0
+    add: (l1, l2, l3) => add(r, l1, l2, l3)
   };
   return r;
 }
