7fc33c9c7a2cc102407852d9e09a72279c6397f1
Bjorn Reppen <bjornreppen@users.noreply.github.com>
2019-08-23 12:46:14 +0200

    Tweak formatet

--- steg/04_typer.js
+++ steg/04_typer.js
@@ -1,4 +1,5 @@
 const lastejobb = require("lastejobb");
+const log = lastejobb.log;
 const fs = require("fs");
 var parseString = require("xml2js").parseString;
 
@@ -7,7 +8,11 @@ const autor2kode = lastejobb.io.lesDatafil("autor2kode");
 // Ustabil link, manuell download: https://register.geonorge.no/register/versjoner/produktspesifikasjoner/kartverket/stedsnavn-for-vanlig-bruk (GML-skjema)
 const xsd = fs.readFileSync("kildedata/StedsnavnForVanligBruk.xsd", "utf8");
 
-const r = {};
+const r = {
+  SN: {
+    tittel: { nb: "Stedsnavn" }
+  }
+};
 
 parseString(xsd, function(err, result) {
   const schema = result.schema;
@@ -34,10 +39,13 @@ function mapEnum(t, nivå) {
     };
     if (src.annotation) {
       const doc = src.annotation[0].documentation[0].split(":");
-      if (doc[1]) e.ingress = doc[1].trim();
-      e.tittel = doc[0].trim();
+      if (doc[1]) e.ingress = doc[1].trim().replace(/\s\s/g, " ");
+      e.tittel = { nb: doc[0].trim() };
     }
-    if (r[e.kodeautor]) throw new Error();
-    r[e.kodeautor] = e;
+    if (!e.kode) return log.warn("Mangler typen " + kodeautor);
+
+    if (r[e.kode]) throw new Error("Duplikat " + e.kode);
+    r[e.kode] = e;
+    delete e.kode;
   });
 }
