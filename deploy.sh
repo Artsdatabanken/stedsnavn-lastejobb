a2a3f6a1e9a5d6ce9cea18ab7877f0ba96999489
Bjorn Reppen <bjornreppen@users.noreply.github.com>
2019-08-24 16:56:46 +0200

    Deploy and restart

--- deploy.sh
+++ deploy.sh
@@ -1,2 +1,3 @@
 # deploy full-text-index
 scp build/full-text-index-sted.json grunnkart@hydra:/dockerdata/generic-substring-lookup-api/full-text-index-sted.json
+ssh grunnkart@hydra docker restart generic-substring-lookup-api
