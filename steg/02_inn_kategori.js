cfb8387804150efb0207885ce45ef2215086364f
Bjorn Reppen <bjornreppen@users.noreply.github.com>
2019-08-25 15:52:22 +0200

    Importer kategorier

---
+++ steg/02_inn_kategori.js
@@ -0,0 +1,26 @@
+const oboe = require("oboe");
+const fs = require("fs");
+const lastejobb = require("lastejobb");
+
+let count = 1;
+
+const kategori = {};
+
+function add(l1, l2, l3) {
+  if (!kategori[l1]) kategori[l1] = {};
+  const x1 = kategori[l1];
+  if (!x1[l2]) x1[l2] = {};
+  const x2 = x1[l2];
+  if (x2[l3]) return x2[l3];
+  x2[l3] = ++count;
+}
+
+oboe(fs.createReadStream("./data/4326.geojson", { encoding: "utf8" }))
+  .node("features.*", function(e) {
+    const p = e.properties;
+    add(p.navneobjekthovedgruppe, p.navneobjektgruppe, p.navneobjekttype);
+    return oboe.drop;
+  })
+  .done(() => {
+    lastejobb.io.skrivDatafil(__filename, kategori);
+  });
