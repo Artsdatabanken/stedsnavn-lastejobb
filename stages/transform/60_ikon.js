17f187073164e812265d95dcc10124449e7109cc
Bjorn Reppen <bjornreppen@users.noreply.github.com>
2020-10-07 18:10:09 +0200

    Nye filnavn i Material icons 48px=>24px

--- stages/transform/60_ikon.js
+++ stages/transform/60_ikon.js
@@ -1,5 +1,5 @@
 const { io } = require("lastejobb");
 
 let ikoner = io.findFiles("temp/icons", ".svg");
-ikoner = ikoner.filter(x => x.indexOf("48px.svg") > 0);
+ikoner = ikoner.filter(x => x.indexOf("24px.svg") > 0);
 io.skrivDatafil("material", ikoner);
