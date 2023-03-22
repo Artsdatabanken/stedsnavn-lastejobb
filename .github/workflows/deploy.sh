9dae766cd225401b73fc0fc53f2f8c07cd3feb37
Myrdrahl <even.juberg@artsdatabanken.no>
2023-03-22 12:18:03 +0100

    strange file sizes

--- .github/workflows/deploy.sh
+++ .github/workflows/deploy.sh
@@ -2,4 +2,5 @@ cp build/steder_kategori.json destinationRepo/
 cp build/steder_kategori.schema.json destinationRepo/
 cp build/type.json destinationRepo/
 cp build/type.schema.json destinationRepo/
+cat build/type.json
 tar cf artifacts.tar build
