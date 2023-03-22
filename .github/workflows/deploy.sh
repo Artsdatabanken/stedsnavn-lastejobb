781887635d463abf0c5a45d736439bf4442cc165
Myrdrahl <even.juberg@artsdatabanken.no>
2023-03-22 09:27:21 +0100

    artifacts

--- .github/workflows/deploy.sh
+++ .github/workflows/deploy.sh
@@ -2,3 +2,4 @@ cp build/steder_kategori.json destinationRepo/
 cp build/steder_kategori.schema.json destinationRepo/
 cp build/type.json destinationRepo/
 cp build/type.schema.json destinationRepo/
+tar cf artifacts.tar build
