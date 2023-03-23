8b6240a60858d05c1a693fb3c44fa70f46d2370b
Myrdrahl <even.juberg@artsdatabanken.no>
2023-03-23 14:48:22 +0100

    pwd

--- stages/transform/02_unzip.js
+++ stages/transform/02_unzip.js
@@ -5,7 +5,7 @@ const fs = require("fs");
 archive.unzip("stedsnavn.zip");
 
 const basisUnzipped = "./temp/Basisdata_0000_Norge_25833_Stedsnavn_GML.gml";
-let pwd = process.env.pwd;
+let pwd = process.env.PWD;
 log.info("Current directory: " + pwd)
 let basisInfo = fs.statSync(pwd + "/" + basisUnzipped);
 let basisSize = basisInfo.size / (1024*1024);
