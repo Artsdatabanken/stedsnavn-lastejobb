03078cdc272df1c6bdbad8fb7718cc061cfe11ed
Myrdrahl <even.juberg@artsdatabanken.no>
2023-03-23 14:00:20 +0100

    unzipped fileSize?

--- stages/transform/02_unzip.js
+++ stages/transform/02_unzip.js
@@ -1,3 +1,9 @@
 const { archive } = require("@artsdatabanken/lastejobb");
+const log = require("@artsdatabanken/lastejobb/lib/log");
 
 archive.unzip("stedsnavn.zip");
+
+const basisUnzipped = "./temp/Basisdata_0000_Norge_25833_Stedsnavn_GML.gml";
+let basisInfo = fs.statSync(basisUnzipped);
+let basisSize = basisInfo.size / (1024*1024);
+log.info("Basisdata size: "+ basisSize);
