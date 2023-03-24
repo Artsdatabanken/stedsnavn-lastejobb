5954d1e829ce94aef6d16afeee9e2374565efd1d
stigat <stig.thorseth@artsdatabanken.no>
2023-03-24 15:24:40 +0100

    remove before clone, unzip instead of archive module

--- stages/download/material-icons.js
+++ stages/download/material-icons.js
@@ -1,3 +1,13 @@
 const { git } = require("@artsdatabanken/lastejobb");
+const { processes } = require("@artsdatabanken/lastejobb/lib");
 
+let pwd = process.env.PWD;
+try{
+let cmd = `rm -rf ${pwd}/temp/icons`;
+console.log("cmd var: ", cmd);
+processes.exec(cmd);
+}catch(e){
+    console.log("No directory to remove so ok i guess...continuing")
+}
 git.clone("https://github.com/google/material-design-icons/", "temp/icons");
+
--- stages/transform/02_unzip.js
+++ stages/transform/02_unzip.js
@@ -1,11 +1,15 @@
 const { archive } = require("@artsdatabanken/lastejobb");
+const { processes } = require("@artsdatabanken/lastejobb/lib");
 const log = require("@artsdatabanken/lastejobb/lib/log");
 const fs = require("fs");
 
-archive.unzip("stedsnavn.zip");
 
-const basisUnzipped = "/temp/Basisdata_0000_Norge_25833_Stedsnavn_GML.gml";
 let pwd = process.env.PWD;
+//archive.unzip("stedsnavn.zip");
+console.log("pwd var: "+pwd);
+processes.exec(`unzip -u ./temp/stedsnavn -d ${pwd}/temp/`);
+const basisUnzipped = "/temp/Basisdata_0000_Norge_25833_Stedsnavn_GML.gml";
+
 log.info("Current directory: " + pwd)
 
 try {
