f0a9f62b0ac32c89363c39ef6158adbf13d6a82d
Myrdrahl <even.juberg@artsdatabanken.no>
2023-03-27 12:35:21 +0200

    cleaning up

--- stages/download/stedsnavn.js
+++ stages/download/stedsnavn.js
@@ -1,5 +1,4 @@
 const { http, log } = require("@artsdatabanken/lastejobb");
-const fs = require("fs");
 
 http
   .downloadBinary(
@@ -9,11 +8,6 @@ http
   .catch(err => {
     log.fatal(err);
   });
-
-const stedsnavn = "./temp/stedsnavn.zip";
-let stedsnavnInfo = fs.statSync(stedsnavn);
-let stedsnavnSize = stedsnavnInfo.size / (1024*1024);
-log.info("Zip file size: " + stedsnavnSize);
 
 //const basisUnzipped = "./temp/Basisdata_0000_Norge_25833_Stedsnavn_GML.gml";
 
